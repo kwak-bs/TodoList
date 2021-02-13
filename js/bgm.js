15b9aa064d4b7644809f76fd21f0ee0896135553
kwak-bs <iam-bs@naver.com>
2021-02-14 02:30:02 +0900

    FEAT : music dark mode 기능 추가

--- js/bgm.js
+++ js/bgm.js
@@ -3,13 +3,7 @@ window.addEventListener("load", () => {
   const musics = document.querySelectorAll(".musics div");
   const effects = document.querySelector(".music__effects");
   const title = document.querySelector(".music__title");
-  const colors = [
-    "rgb(178, 32, 112)",
-    "rgb(214, 141, 30)",
-    "rgb(64, 55, 196)",
-    "rgb(4, 184, 13)",
-    "rgb(216, 55, 189)",
-  ];
+
   musics.forEach((music, index) => {
     music.addEventListener("click", function () {
       audios.forEach((idx) => {
@@ -30,15 +24,19 @@ window.addEventListener("load", () => {
         title.innerHTML = strArr[1];
       }
 
-      createAnimations(index);
+      createAnimations(index, music);
     });
   });
 
   const createAnimations = (index) => {
+    const color = getComputedStyle(document.documentElement).getPropertyValue(
+      `--music-${index + 1}`
+    );
+
     effects.innerHTML = "";
     const bubble = document.createElement("div");
     effects.appendChild(bubble);
-    bubble.style.backgroundColor = colors[index];
+    bubble.style.backgroundColor = color;
     bubble.style.animation = "animation 2000ms linear infinite both";
     bubble.style.position = "relative";
   };
